11497426e595b641f5c4652653996f4862b5b66a
Aibika <sabyraibika@gmail.com>
2025-01-31 18:33:53 +0600

    Create login form

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,10 +1,31 @@
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 
 export const Login = () => {
+  const [idInput, setIdInput] = useState('');
+  const [passwordInput, setPasswordInput] = useState('');
   const [currentStepIndex, setCurrentStepIndex] = useState(0);
+  const [isCheckboxChecked, setIsCheckboxChecked] = useState(false);
+  const [passwordInputType, setPasswordInputType] = useState<
+    'text' | 'password'
+  >('password');
+
   const handleStepBack = () => {
     setCurrentStepIndex((prev) => prev - 1);
   };
+
+  const togglePasswordVisibility = () => {
+    if (passwordInputType === 'password') {
+      setPasswordInputType('text');
+    } else {
+      setPasswordInputType('password');
+    }
+  };
+
+  const onFormSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log({ id: idInput, password: passwordInput });
+  };
+
   const steps = [
     {
       content: (
@@ -30,23 +51,101 @@ export const Login = () => {
         <div className='mt-[101px] w-full px-6'>
           <nav className='flex items-center'>
             <h1 className='text-lg font-bold leading-6 relative w-full flex items-center justify-center'>
-              <button onClick={handleStepBack} className='absolute left-0 flex justify-center items-center'>
+              <button
+                onClick={handleStepBack}
+                className='absolute left-0 flex justify-center items-center cursor-pointer'
+              >
                 <img src='/arrow-left.svg' alt='' />
               </button>
               <span>Login</span>
             </h1>
           </nav>
-          <div className='flex flex-col gap-2 mt-[80px]'>
-            <input type='text' className='input-primary' placeholder='ID' />
-            <input
-              type='text'
-              className='input-primary'
-              placeholder='Password'
-            />
-            <p className='text-sm leading-5 text-platinum-500'>
-              Make sure it matches the name on your university ID.
-            </p>
+          <form id='login-form' onSubmit={onFormSubmit}>
+            <div className='flex flex-col gap-2 mt-[80px]'>
+              <input
+                type='text'
+                className='input-primary'
+                placeholder='ID'
+                required
+                value={idInput}
+                onChange={(e) => setIdInput(e.target.value)}
+              />
+              <div className='relative flex items-center'>
+                <input
+                  type={passwordInputType}
+                  className='input-primary w-full'
+                  placeholder='Password'
+                  required
+                  value={passwordInput}
+                  onChange={(e) => setPasswordInput(e.target.value)}
+                />
+                <span
+                  className='absolute right-4 cursor-pointer'
+                  onClick={togglePasswordVisibility}
+                >
+                  <img src='/eye.svg' alt='' />
+                </span>
+              </div>
+              <p className='text-sm leading-5 text-platinum-500'>
+                Make sure it matches the name on your university ID.
+              </p>
+            </div>
+          </form>
+          <div className='flex flex-col mt-[200px]'>
+            <div className='group grid size-7 grid-cols-1'>
+              <input
+                type='checkbox'
+                name='terms-agree'
+                id='terms-agree'
+                className='col-start-1 row-start-1 appearance-none rounded-sm border border-purple-500 bg-white checked:border-purple-500 checked:bg-purple-500 indeterminate:border-purple-500 indeterminate:bg-purple-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-500 disabled:border-gray-300 disabled:bg-gray-100 disabled:checked:bg-gray-100 forced-colors:appearance-auto cursor-pointer'
+                onChange={(e) => setIsCheckboxChecked(e.target.checked)}
+                checked={isCheckboxChecked}
+              />
+              <svg
+                fill='none'
+                viewBox='0 0 14 14'
+                className='pointer-events-none col-start-1 row-start-1 size-5 self-center justify-self-center stroke-white group-has-disabled:stroke-gray-950/25'
+              >
+                <path
+                  d='M3 8L6 11L11 3.5'
+                  strokeWidth={1.5}
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  className='opacity-0 group-has-checked:opacity-100'
+                />
+                <path
+                  d='M3 7H11'
+                  strokeWidth={1.5}
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  className='opacity-0 group-has-indeterminate:opacity-100'
+                />
+              </svg>
+            </div>
+            <label htmlFor='terms-agree' className='text-sm leading-5 mt-2'>
+              By selecting Agree and continue, I agree to Dynamic Layers{' '}
+              <a href='#' className='text-purple-500 underline font-bold'>
+                Terms of Service
+              </a>
+              ,{' '}
+              <a href='#' className='text-purple-500 underline font-bold'>
+                Notification Policy
+              </a>{' '}
+              and acknowledge the{' '}
+              <a href='#' className='text-purple-500 underline font-bold'>
+                Privacy Policy
+              </a>
+              .
+            </label>
           </div>
+          <button
+            className='btn-primary w-full mt-4'
+            type='submit'
+            form='login-form'
+            disabled={!isCheckboxChecked}
+          >
+            Agree and continue
+          </button>
         </div>
       ),
     },
